baa31e1054bfec164e3520f66f7f5187f5884305
Michael <mquinter88@gmail.com>
2024-03-13 15:42:14 -0400

    init

---
+++ src/main/java/org/mcq/ecommercesvc/EcommerceSvcApplication.java
@@ -0,0 +1,13 @@
+package org.mcq.ecommercesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mcq/ecommercesvc/EcommerceSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mcq.ecommercesvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
